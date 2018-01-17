11241522dfe961529ce71a18f5fc9d72a9cd7859
Kyle Chambers <kylejchambers@gmail.com>
2018-01-17 15:14:12 -0800

    passing through functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,30 @@
 var Stack = function() {
   var someInstance = {};
+  var length = 0;
+
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    length += arguments.length;
+    storage[length -1] = value;
+    return value;
   };
 
   someInstance.pop = function() {
+    var save;
+    if (length){
+      save = storage[length -1];
+      delete storage[length -1];
+      length--;
+    }
+    return save;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
