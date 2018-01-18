52aa60b49ce4dc120c20f83013d1dfe16eca7da5
Kyle Chambers <kylejchambers@gmail.com>
2018-01-17 18:11:06 -0800

    passing tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -8,7 +8,7 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
-    length += arguments.length;
+    length++;
     storage[length -1] = value;
     return value;
   };
