3a70451e4eb54b1864d7929f1f03d6fc9d9df214
Kyle Chambers <kylejchambers@gmail.com>
2018-01-17 16:37:24 -0800

    making progress, passing through functional shared push

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  //   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  var length = 0;
+  var storage = {};
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function () {
+  return this.length || 0;
+};
 
+stackMethods.push = function(value){
+  if (this.storage){
+    this.storage[length] = value;
+  } else {
+    this.storage = {};
+    this.storage[length] = value;
+  }
+
+  if (this.length){
+    this.length++;
+  } else {
+    this.length = 1;
+  }
+
+  return value;
+};
+
+stackMethods.pop = function(){
+  if ()
+};
