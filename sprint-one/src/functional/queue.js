0cd139fba8481b78fb665778ef9ba14d85b67724
Kyle Chambers <kylejchambers@gmail.com>
2018-01-18 08:55:28 -0800

    cleaning up white space

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,29 @@
 var Queue = function() {
   var someInstance = {};
-
+  var length = 0;
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    length++;
+    storage[length -1] = value;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    var save = storage[0];
+    for (var index in storage){
+      storage[index -1] = storage[index];
+    }
+    if (length){
+      length--;
+    }
+    delete storage[-1];
+    return save;
   };
 
   someInstance.size = function() {
+    return length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,11 +1,8 @@
 var Stack = function() {
   var someInstance = {};
   var length = 0;
-
-
   // Use an object with numeric keys to store values
   var storage = {};
-
   // Implement the methods below
   someInstance.push = function(value) {
     length++;
