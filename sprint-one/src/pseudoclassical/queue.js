cf5c61cecc47bd5773909823015901f18343a826
Kyle Chambers <kylejchambers@gmail.com>
2018-01-18 11:30:14 -0800

    passing tests through stack es6

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -14,3 +14,17 @@ Queue.prototype.enqueue = function(value) {
   this.storage[this.length -1] = value;
   return value;
 };
+
+Queue.prototype.dequeue = function() {
+  var save = this.storage[0];
+  for (var index in this.storage){
+    this.storage[index -1] = this.storage[index];
+  }
+
+  if (this.length){
+    this.length--;
+  }
+
+  delete this.storage[-1];
+  return save;
+};
